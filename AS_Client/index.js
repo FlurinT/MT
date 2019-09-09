68475cf88f7fb5f6b615fc65067eca9aab5a82c6
FLurinT <flurin.truebner@students.unibe.ch>
2019-09-09 15:29:08 +0200

    working with static keys now

--- AS_Client/index.js
+++ AS_Client/index.js
@@ -3,6 +3,17 @@ const encodr = require('encodr')
 const cose = require('cose-js')
 const EC = require('elliptic').ec
 
+let ec = new EC('p256').genKeyPair()
+/*
+console.log('Private   ' + ec.getPrivate().toString('hex'))
+console.log('Public X  '+ ec.getPublic().getX().toString('hex'))
+console.log('Public Y  '+ ec.getPublic().getY().toString('hex'))
+*/
+
+let private = 'eccaba7c265cbad6d605e1bc917f95e634d36bf12c4204832d10541f4f001462'
+let publicX = '1b698d23537b54c9b8098e81aa2317bfa0aa22197ebe334ed624d0a719c26689'
+let publicY = 'f830a16268f812f2762367783ccf6fa7312e189f5f6813a0aa928ecf3eb9e46f'
+
 const plaintext = JSON.stringify({
     client_id: 'client_0',
     audience: 'temp',
@@ -17,11 +28,9 @@ const headers = {
   'p': {'alg': 'ES256'},
   'u': {'kid': '11'}
 };
-
-let ec = new EC('p256').genKeyPair()
 const signer = {
   'key': {
-    'd': Buffer.from(ec.getPrivate().toString('hex'), 'hex')
+    'd': Buffer.from(private, 'hex')
   }
 };
 
@@ -36,8 +45,8 @@ cose.sign.create(
 .then((buf) => {
     const verifier = {
         'key': {
-          'x': Buffer.from(ec.getPublic().getX().toString('hex'), 'hex'),
-          'y': Buffer.from(ec.getPublic().getY().toString('hex'), 'hex')
+          'x': Buffer.from(publicX, 'hex'),
+          'y': Buffer.from(publicY, 'hex')
         }
       }; 
     cose.sign.verify(
