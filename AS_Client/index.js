b4328ca0f987ced4f58ede783974c1a3bf189e67
FLurinT <flurin.truebner@students.unibe.ch>
2019-09-09 16:18:31 +0200

    added statit ephemeral keys which makes zero sense :)

--- AS_Client/index.js
+++ AS_Client/index.js
@@ -4,6 +4,7 @@ const cose = require('cose-js')
 const EC = require('elliptic').ec
 
 let ec = new EC('p256').genKeyPair()
+
 /*
 console.log('Private   ' + ec.getPrivate().toString('hex'))
 console.log('Public X  '+ ec.getPublic().getX().toString('hex'))
@@ -14,14 +15,21 @@ let private = 'eccaba7c265cbad6d605e1bc917f95e634d36bf12c4204832d10541f4f001462'
 let publicX = '1b698d23537b54c9b8098e81aa2317bfa0aa22197ebe334ed624d0a719c26689'
 let publicY = 'f830a16268f812f2762367783ccf6fa7312e189f5f6813a0aa928ecf3eb9e46f'
 
+let privateEPH = 'c81e9dc0b03170e80e2ba99d8a20b526d78e7b9848c624a48755fec77281c528'
+let publicXEPH = '1ae92174574cd039c7b1a1dab863efdd08fe4fdc7a78904789a98636b98d9a46'
+let publicYEPH = 'd022fcc96289290431c7e8cda295d953e08fdcc450a85b0e2ce869b19101b507'
+
 const plaintext = JSON.stringify({
     client_id: 'client_0',
     audience: 'temp',
     req_cnf:{
-        kty: 'EC',
-        kid: "h'11'",
-        x: 1,
-        y:2
+        COSE_Key: {
+            kty: 'EC',
+            kid: "h'11'",
+            crv: 'P-256',
+            x: publicX,
+            y: publicY
+        }
     }
 })
 const headers = {
