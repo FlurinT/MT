90fe9b1fa1ed647375f7a95664357193a1d86c1e
FLurinT <flurin.truebner@students.unibe.ch>
2019-09-27 12:10:54 +0200

    generate Eph keys in mockClient constructor for the worklfow

--- test/workflowTest.js
+++ test/workflowTest.js
@@ -1,32 +1,27 @@
 'use strict'
 const coap = require('coap')
 const coap_router = require('../coap_router')
-const Client = require('../client').Client
+const Client = require('../testObjects/mockClient').MockClient
 const assert = require('assert')
 const cbor = require('cbor')
 
-describe('#fullWorkFlow', () => {
+describe('#Client-AS', () => {
 
     before(() => {
         const server = coap.createServer(coap_router)
             server.listen(() => {
                 console.log('CoAP server is up for testing')
             })
-        this.asAdress = 'coap://localhost'
-        this.rsAdress = 'coap://localhost'
     })
 
     it('testTokenEndpoint', () => {
-        var client = new Client(this.asAdress, this.rsAdress)
+        var client = new Client('coap://localhost', 'coap://localhost')
         client.buildRequest()
         .then((req) => {
             req.on('response', (res) => {
                 const b64token = res.payload.toString()
                 const tokenPayload = (Buffer.from(b64token, 'base64').slice(2))
                 assert.equal(res.code, '2.05')
-                console.log('tokkken')
-                console.log(tokenPayload.toString('hex'))
-                this.token = tokenPayload
             })
             req.end()
         })
--- testObjects/mockClient.js
+++ testObjects/mockClient.js
@@ -1,14 +1,22 @@
 const coap = require('coap')
 const EC = require('elliptic').ec
 const cbor = require('cbor')
-const coseHelper = require('./services/coseHelper')
+const coseHelper = require('../services/coseHelper')
 const cose = require('cose-js')
 const base64url = require('base64url')
 
-class Client {
-    constructor(asAdress, rsAdress) {
+class MockClient {
+    constructor(asAdress, rsAdress, privateK, publicX, publicY) {
         this.asAdress = asAdress
         this.rsAdress = rsAdress
+        this.privateK = privateK
+        this.publicX = publicX
+        this.publicY = publicY
+
+        let ec = new EC('p256').genKeyPair()
+        this.privateEPH = ec.getPrivate().toString('hex')
+        this.publicXEPH = ec.getPublic().getX().toString('hex')
+        this.publicYEPH = ec.getPublic().getY().toString('hex')
     }
 
     async buildRequest() {
@@ -17,9 +25,6 @@ class Client {
         var privateK = 'eccaba7c265cbad6d605e1bc917f95e634d36bf12c4204832d10541f4f001462'
         var publicX = '1b698d23537b54c9b8098e81aa2317bfa0aa22197ebe334ed624d0a719c26689'
         var publicY = 'f830a16268f812f2762367783ccf6fa7312e189f5f6813a0aa928ecf3eb9e46f'
-        let privateEPH = 'c81e9dc0b03170e80e2ba99d8a20b526d78e7b9848c624a48755fec77281c528'
-        let publicXEPH = '1ae92174574cd039c7b1a1dab863efdd08fe4fdc7a78904789a98636b98d9a46'
-        let publicYEPH = 'd022fcc96289290431c7e8cda295d953e08fdcc450a85b0e2ce869b19101b507'
 
         const requestPayload = {
             client_id: 'client0',
@@ -29,8 +34,8 @@ class Client {
                     kty: 'EC',
                     kid: "h'11'",
                     crv: 'P-256',
-                    x: publicXEPH,
-                    y: publicYEPH
+                    x: this.publicXEPH,
+                    y: this.publicYEPH
                 }
             }
         }
@@ -65,4 +70,4 @@ class Client {
     }
 }
 
-module.exports.Client = Client
+module.exports.MockClient = MockClient
