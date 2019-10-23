1b5308e8b726fa540a65a13f71ba8b9ab83ab1e8
FLurinT <flurin.truebner@students.unibe.ch>
2019-10-23 17:06:40 +0200

    working interface to deploy and call contracts as DPKI class in dpkiapi.js

--- DPKI/dpkiapi.js
+++ DPKI/dpkiapi.js
@@ -1,29 +1,35 @@
 const Web3 = require('web3')
 const fs = require('fs')
 
-let contractBuffer = fs.readFileSync('./build/contracts/DPKI.json')
-let contractJson = JSON.parse(contractBuffer)
-let abi = contractJson.abi
-let bytecode = contractJson.bytecode
-let contractAddress
-const web3 = new Web3('ws://localhost:8545');
+class DPKI {
+  constructor() {
+    var contractBuffer = fs.readFileSync('./build/contracts/DPKI.json')
+    var contractJson = JSON.parse(contractBuffer)
+    this.abi = contractJson.abi
+    this.bytecode = contractJson.bytecode
+    this.web3 = new Web3('ws://localhost:8545');
+  }
 
-web3.eth.getAccounts()
-.then((accounts) => {
-  const Contract = new web3.eth.Contract(
-    abi,
-    {data: bytecode, from: accounts[0], gas: 5000000}
-  )
-
-  Contract.deploy()
-  .send()
-  .on('receipt', (receipt) => {
-      contractAddress = receipt.contractAddress
-      console.log('CONTRACT ADDRESS')
-      console.log(contractAddress)
-
-      const DPKI = new web3.eth.Contract(abi, contractAddress)
-      DPKI.methods.createKeyRing(1,2,3,).send({from: accounts[0], gas: '500000'})
-      console.log('done')
+  async deployContract() {
+    this.accounts = await this.web3.eth.getAccounts()
+    const contract = await new this.web3.eth.Contract(
+      this.abi, { data: this.bytecode, from: this.accounts[0], gas: 5000000 }
+    )
+    return new Promise((resolve) => {
+      contract.deploy().send()
+        .on('receipt', (receipt) => {
+          var contractAddress = receipt.contractAddress
+          const DPKI = new this.web3.eth.Contract(this.abi, contractAddress)
+          resolve(DPKI)
+        })
     })
-})
+  }
+}
+
+async function test() {
+  const dpki = await new DPKI()
+  var contract = await dpki.deployContract()
+  contract.methods.get().call({ from: dpki.accounts[0] }).then(console.log)
+}
+
+test()
